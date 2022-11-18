22a96a811e5c867d2e5f9293137de9c3e51db114
Juan Miguel Salvatierra <ignitor31@icloud.com>
2022-11-18 10:31:35 +0100

    .

--- InfinityRunWebHTML/main.js
+++ InfinityRunWebHTML/main.js
@@ -4,7 +4,10 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 48.22176299638565, lng: 16.445311903684694 },
     zoom: 17,
+    disableDefaultUI: true,
   });
+
+  map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
 
   const uluru = { lat: 48.22176299638565, lng: 16.445311903684694 };
 
